a1867ada3412e19daa6c88a5dd86407eec1696a3
GustavoMourao <gustavolmourao@gmail.com>
2018-03-05 17:21:47 -0300

    project_online_interface

---
+++ online_proc/Vocoder/main_gammatoneSpec.m
@@ -0,0 +1,49 @@
+% 
+% This script demonstrates the use of the CI Vocoder 
+% from Bräcker, T. et al. (2009): Simulation und Vergleich von
+% Sprachkodierungsstrategien in Cochlea-Implantaten. 
+% Zeitschrift der Audiologie, 48(4):158-169.
+% 
+% Author:   Florian Langner
+% Version:  1.00
+% Date:     12.03.15
+% 
+% Modified: Gustavo Mourao (03/02/2018)
+
+clc; clear all; close all;
+addpath('CI Vocoder');
+
+% Load the wav files into Matlab
+% Clean
+[Original, fs] = audioread('F0002007.wav');
+lenOr = length(Original);
+% Unprocessed - HORRIBLE! Change to SNR = 0dB, then to +5dB
+% [Original, fs] = audioread('F0002007_S0N90_SNR0_Un.wav');
+% Original = Original(1:lenOr,2);
+% MMSE
+% [Original, fs] = audioread('F0002007_S0N90_SNR0_MMSE.wav');
+% Original = Original(1:lenOr,2);
+% Wiener
+% [Original, fs] = audioread('F0002007_S0N90_SNR0_Wiener.wav');
+% Original = Original(1:lenOr,2);
+% Bnary Mask
+[Original, fs] = audioread('F0002007_S0N90_SNR0_Binary Mask.wav');
+Original = Original(1:lenOr,2);
+
+% Use the Vocoder with CI simulation and the MED-EL simulation
+% Vocoded_Male_MEDEL = CI_Vocoder_MEDEL(Original_Male, fs, 'CI');
+% Vocoded_Female_MEDEL = CI_Vocoder_MEDEL(Original_Female, fs, 'CI');
+
+% Use the Vocoder with CI simulation and the Cochlear Nucleus simulation
+[Vocoded_Male_Cochlear, electrodeband] = CI_Vocoder_Cochlear(Original, fs, 'CI');
+% Vocoded_Female_Cochlear = CI_Vocoder_Cochlear(Original_Female, fs, 'CI');
+
+% Preperation and presentation of original and vocoded signal
+Silence = zeros(fs, 1)';
+% Presentation = [Original_Male' Silence Vocoded_Male_MEDEL' Silence Vocoded_Male_Cochlear' Silence ...
+%                 Original_Female' Silence Vocoded_Female_MEDEL' Silence Vocoded_Female_Cochlear'];
+Presentation = [Vocoded_Male_Cochlear'];
+player = audioplayer(Presentation, fs);
+play(player);
+
+% End of File
