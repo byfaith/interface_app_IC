597f5f5ea699f37baf0998a6204f404c45f5c263
GustavoMourao <gustavolmourao@gmail.com>
2018-03-07 08:45:09 -0300

    project_online_interface_update_data

---
+++ online_proc/Data/CIusers/main__getData.m
@@ -0,0 +1,59 @@
+% Get SRT50 
+clear all
+clc
+%% MMSE
+addpath('Data\CIusers\Marina')
+load('Marina_MMSE.mat')
+
+index = find(resultados.numTotalPalavras);
+snr = resultados.snr_vecValues(index);
+wrc = resultados.numAcertos(index)./resultados.numTotalPalavras(index);
+
+figure;plot(snr)
+hold on
+title('MMSE')
+xlabel('Trial number')
+ylabel('SNR [dB]')
+
+%% Wiener
+addpath('Data\CIusers\Marina')
+load('Marina_Wiener.mat')
+
+index = find(resultados.numTotalPalavras);
+snr = resultados.snr_vecValues(index);
+wrc = resultados.numAcertos(index)./resultados.numTotalPalavras(index);
+
+figure;plot(snr)
+hold on
+title('Wiener')
+xlabel('Trial number')
+ylabel('SNR [dB]')
+
+
+%% Binary Mask
+addpath('Data\CIusers\Marina')
+load('Marina_Binary.mat')
+
+index = find(resultados.numTotalPalavras);
+snr = resultados.snr_vecValues(index);
+wrc = resultados.numAcertos(index)./resultados.numTotalPalavras(index);
+
+figure;plot(snr)
+hold on
+title('Binary Mask')
+xlabel('Trial number')
+ylabel('SNR [dB]')
+
+%% Unproc
+addpath('Data\CIusers\Marina')
+load('Marina_Un.mat')
+
+index = find(resultados.numTotalPalavras);
+snr = resultados.snr_vecValues(index);
+wrc = resultados.numAcertos(index)./resultados.numTotalPalavras(index);
+
+figure;plot(snr)
+hold on
+title('Unprocessed')
+xlabel('Trial number')
+ylabel('SNR [dB]')
